b84ffb0ece08ec829ca79862e0c4df5631367d5e
Henrique Vaz <henriquevgvaz@gmail.com>
2018-02-26 17:56:30 -0500

    including format_intervals and changing candles formatting function

--- exchanges.py
+++ exchanges.py
@@ -5,6 +5,7 @@ from markets import Market
 from candles import Candles
 import info_exchanges
 
+NUM_CANDLES = 2
 
 class Exchange(object):
 
@@ -24,59 +25,77 @@ class Exchange(object):
                 self.markets[s_name] = Market(s_name, self.percentage_fee)
 
 
-    def __format_candles_binance(self, candles):
-
-        new_candles = []
-        for c in candles:
-            
-            c_new = {}
-            c_new['open_tstamp'] = int(c[0])
-            c_new['close_tstamp'] = int(c[6])
-            c_new['open_price'] = float(c[1])
-            c_new['close_price'] = float(c[4])
-            c_new['min_price'] = float(c[3])
-            c_new['max_price'] = float(c[2])
-            c_new['volume'] = float(c[5])
-            c_new['volume_quote'] = float(c[7])
-            new_candles.append(c_new)
-
-        return new_candles
-
-
     def __format_candles(self, candles):
-
+        
+        equivalence = {}
         if (self.name == 'binance'):
-            return self.__format_candles_binance(candles)
-        else:
-            return None
 
-    
-    def __get_candles_data(self, mkt_name, interval):
+            equivalence = {'open_tstamp': 0, 'close_tstamp': 6, 'open_price': 1, 'close_price': 4, \
+                           'min_price': 3, 'max_price': 2, 'volume': 5, 'volume_quote': 7}
+
+        elif: (self.name == 'hitbtc'):
+
+            equivalence = {'open_tstamp': None, 'close_tstamp': 'timestamp', 'open_price': 'open', \
+                           'close_price': 'close', 'min_price': 'min', 'max_price': 'max', \
+                           'volume': 'volume', 'volume_quote': 'volumeQuote'}
+
+        else:
+            raise 'Cannot format candles for this exchange yet. Please update code.'
+            return None
+   
+
+        formatted_candles = [{param: c[index] if index != None else param: None 
+                              for param, index in equivalence.items()}
+                            for c in candles]
+
+        return formatted_candles
+
+
+    def __get_candles_data(self, mkt_name, interval, target):
 
         filename = 'exchange=' + self.name + '_mkt=' + mkt_name + '_data=candles_interval=' + interval + '.txt'
         with open(filename, 'a') as f:
             
             with requests.Session() as session:
                 
-                payload = {'symbol': mkt_name, 'interval': interval}
-                url = self.api_endpoint + self.candles_endpoint
-                r = session.get(url, params=payload)
+                candles_count = 0
+                while (candles_count < target):
                 
-                json_content = json.loads(r.content)
-                formatted_candles = self.__format_candles(json_content)
-                if (formatted_candles == None):
-                    raise 'Cannot format candles for this exchange yet. Please implement method'
-                    return
+                    payload = {'symbol': mkt_name, 'interval': interval, 'limit': 2}
+                    url = self.api_endpoint + self.candles_endpoint
+                    r = session.get(url, params=payload)
+                    
+                    json_content = json.loads(r.content)
+                    formatted_candles = self.__format_candles(json_content)
+                    if (formatted_candles == None):
+                        return
 
-                print "Number of candles: {}".format(len(formatted_candles))
+                    print "Number of candles: {}".format(len(formatted_candles))
 
-                if (r.status_code != 200):
-                    raise r.raise_for_status()
+                    if (r.status_code != 200):
+                        raise r.raise_for_status()
 
-                f.write(str(formatted_candles))
+                    f.write(str(formatted_candles))
 
 
-    def update_candles(self, mkt_name, interval):
+    def __format_intervals(self, size, unit):
+
+        if (unit not in self.candles_intervals.keys()):
+            raise 'Invalid unit used for candles in this exchange'
+            return
+        
+        possible_intervals = self.candles[unit]
+        possible_sizes = possible_intervals.keys()
+        if (size not in possible_sizes):
+            raise 'Invalid size/unit used for candles in this exchange'
+            return
+
+        return possible_intervals[size]
+
+
+    def update_candles(self, mkt_name, size, unit):
+        
+        interval = self.get_interval(size, unit)
 
         if (mkt_name not in self.markets.keys() or 
             interval not in self.candles_intervals):
@@ -85,7 +104,7 @@ class Exchange(object):
             return
         
         mkt = self.markets[mkt_name]
-        candles_data = self.__get_candles_data(mkt_name, interval)
+        candles_data = self.__get_candles_data(mkt_name, interval, NUM_CANDLES)
         mkt.candles[interval] = Candles(interval, candles_data)
 
         
