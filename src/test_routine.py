05a2892972f587fa93faa1f782b4f81c7f4a54da
Henrique Vaz <henriquevgvaz@gmail.com>
2018-03-14 11:28:25 -0300

    refactoring code

--- src/test_routine.py
+++ src/test_routine.py
@@ -2,6 +2,7 @@ from exchanges import Exchange, NUM_CANDLES
 from infos import info_exchanges, strategies
 from parser import set_parser, handle_args
 from printing import print_comparing_hold
+import logging
 
 def print_env_info(args):
     print "\n\n" + "*" * 100 + "\n"
@@ -44,7 +45,6 @@ def print_model_results(profits, args):
     dots = "." * 11
     arrows = ">" * 10
     print "\nvvvvvvvvvvvv RESULTS vvvvvvvvvvvvvv"
-    ## print results
     for x, mkts_profits in profits.items():
         
         print "\n" + "x" * 100 +  "\n"
@@ -69,7 +69,37 @@ def print_model_results(profits, args):
                     print print_info
 
             print "." * 100 +  "\n"
+
         print "\n"+ "x" * 100 + "\n\n"
+
+
+def apply_model(strategy, args, mkt, interval):
+    cur_candles = mkt.candles[interval]
+    mkt_strategy_profit = 0
+    try:
+        if strategy == "ema":
+            mkt_strategy_profit = mkt.test_ema_model(cur_candles, args["start"], args["end"], \
+                                  args["ema_short"], args["ema_long"], args["ema_threshold"])
+
+        elif strategy == "stat":
+            mkt_strategy_profit = mkt.test_stat_model(cur_candles, args["start"], args["end"], args["stat_buy_th"], \
+                                  args["stat_sell_th"], args["calib_proportion"], updating=args["updating_stat"])[0]
+
+        elif strategy == "hold":
+            mkt_strategy_profit = mkt.test_hold_model(cur_candles, args["start"], args["end"], args["calib_proportion"])
+
+        elif strategy == "study_stats":
+            mkt.study_stats(cur_candles, args["start"], args["end"], args["calib_proportion"], \
+                            updating=args["updating_stat"])
+
+        elif strategy == "sandbox":
+            mkt_strategy_profit = mkt.test_sandbox_model(cur_candles, args["sandbox_params"])
+
+        return mkt_strategy_profit
+
+    except Exception as e:
+        print e
+
 
 def start_testing(args):
     ## instantiate exchanges, update candles, and run strategy
@@ -87,29 +117,14 @@ def start_testing(args):
             interval = new_exchange.format_interval(args["candles_m_size"], args["candles_m_unit"])
             mkt = new_exchange.markets[m]
             
-            mkt_strategy_profit = 0
-            cur_candles = mkt.candles[interval]
-            if (args["strategy"] == "ema"):
-                mkt_strategy_profit = mkt.test_ema_model(cur_candles, args["start"], args["end"], \
-                                      args["ema_short"], args["ema_long"], args["ema_threshold"])
-
-            elif (args["strategy"] == "stat"):
-                mkt_strategy_profit = mkt.test_stat_model(cur_candles, args["start"], args["end"], args["stat_buy_th"], \
-                                      args["stat_sell_th"], args["calib_proportion"], updating=args["updating_stat"])[0]
-
-            elif (args["strategy"] == "sandbox"):
-                mkt_strategy_profit = mkt.test_sandbox_model(cur_candles, args["sandbox_params"])
-            
             exchange_profits[m] = {}
             mkt_profits = exchange_profits[m]
-            mkt_profits[args["strategy"]] = mkt_strategy_profit
+            mkt_profits[args["strategy"]] = apply_model(args["strategy"], args, mkt, interval)
             
             ## run benckmark strategy (holding)
-            hold_profit = mkt.test_hold_model(cur_candles, args["start"], args["end"], args["calib_proportion"])
-            mkt_profits["hold"] = hold_profit
-
-            mkt.study_stats(cur_candles, args["start"], args["end"], args["calib_proportion"], \
-                            updating=args["updating_stat"])
+            mkt_profits["hold"] = apply_model("hold", args, mkt, interval)
+            study_stats = apply_model("study_stats", args, mkt, interval)
+            
 
     return profits
 
