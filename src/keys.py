225e6f52ddd2b2e4be19563fafeeb7d33f87717d
rangelo365 <rangelo365>
2018-03-31 11:19:02 -0400

    removed my keys lol

--- src/keys.py
+++ src/keys.py
@@ -1,7 +1,7 @@
 keys = { \
     "hitbtc2": { \
-        "apiKey": "f778d085656be257b2c6ccb7adb9ec2a", \
-        "secret": "d97022682df232f1b22d874cc123c54c" \
+        "apiKey": "", \
+        "secret": "" \
     }, \
     "binance": { \
         "apiKey": "", \
